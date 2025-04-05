63f8c668c6f5b5405cc9128a8fc0b12c2d44ee46
Lucas Dourado <douradoblucas@gmail.com>
2025-04-05 15:01:29 -0300

    Upload do projeto

---
+++ src/test/java/br/com/lucasbdourado/library/LibraryApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.lucasbdourado.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}

ad2109a9c1ab19947c3d04acfba18aaef328cc1c
Lucas Dourado <douradoblucas@gmail.com>
2025-04-09 15:25:59 -0300

    Adicionado o módulo de Publisher(Editora)

--- src/main/java/br/com/lucasbdourado/library/dto/book/BookResponse.java
+++ src/main/java/br/com/lucasbdourado/library/dto/book/BookResponse.java
@@ -1,10 +1,11 @@
 package br.com.lucasbdourado.library.dto.book;
 
+import br.com.lucasbdourado.library.dto.publisher.PublisherResponse;
 import java.util.GregorianCalendar;
 import java.util.UUID;
 
 public record BookResponse(UUID id, String name, String code, String isbn, GregorianCalendar publishDate,
-                           byte edition, int quantity, short pages, String collection,
+                           byte edition, PublisherResponse publisher, int quantity, short pages, String collection,
                            String description, String series)
 {
 }
--- src/main/java/br/com/lucasbdourado/library/mapper/book/BookMapper.java
+++ src/main/java/br/com/lucasbdourado/library/mapper/book/BookMapper.java
@@ -1,15 +1,19 @@
 package br.com.lucasbdourado.library.mapper.book;
 
 import br.com.lucasbdourado.library.dto.book.BookResponse;
+import br.com.lucasbdourado.library.dto.publisher.PublisherResponse;
 import br.com.lucasbdourado.library.entity.book.Book;
+import br.com.lucasbdourado.library.mapper.publisher.PublisherMapper;
 
 public class BookMapper
 {
 
 	public static BookResponse toResponse(Book book)
 	{
+		PublisherResponse publisherResponse = PublisherMapper.toResponse(book.getPublisher());
+
 		return new BookResponse(book.getId(), book.getName(), book.getCode(), book.getIsbn(),
-			book.getPublishDate(), book.getEdition(), book.getQuantity(), book.getPages(),
+			book.getPublishDate(), book.getEdition(), publisherResponse,  book.getQuantity(), book.getPages(),
 			book.getCollection(), book.getDescription(), book.getSeries());
 	}
 }
